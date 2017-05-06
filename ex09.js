6e35b9bdcadd09f85ffddbc184d4eb49c3309798
Jim Hall <jim.hall@solarmedia.com>
2017-05-06 13:42:23 -0400

    Exercise 9

---
+++ ex09.js
@@ -0,0 +1,39 @@
+const Hapi = require('hapi');
+const Joi = require('joi');
+
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+const schema = Joi.object().keys({
+    breed: Joi.string().required()
+})
+
+server.route([
+    {
+        method: 'GET',
+        path: '/chickens/{breed}',
+        handler: function (request, reply) {
+            reply('Chicken: ' + request.params.breed);
+        },
+        config: {
+            validate: {
+                params: {
+                    breed: Joi.string().required()
+                }
+            }
+        }
+    }
+]);
+
+server.start(function(err) {
+    if (err) throw err;
+    console.log('Server listening at:', server.info.uri);
+})
+
+
+
+
