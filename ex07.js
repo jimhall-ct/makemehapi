7df145166098961e95919fe2e3b1b66f2b14cb86
Jim Hall <jim.hall@solarmedia.com>
2017-05-06 13:06:55 -0400

    Exercise 7

---
+++ ex07.js
@@ -0,0 +1,40 @@
+const Hapi = require('hapi');
+const Vision = require('vision');
+const Path = require('path');
+
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.register(Vision, function(err) {
+    if (err) throw err;
+});
+
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: Path.join(__dirname, '/templates'),
+    helpersPath: Path.join(__dirname, '/helpers')
+});
+
+server.route([
+    {
+        method: 'GET',
+        path: '/',
+        handler: {
+            view: 'index2.html'
+        }
+    }
+]);
+
+server.start(function(err) {
+    if (err) throw err;
+    console.log('Server listening at:', server.info.uri);
+})
+
+
+
