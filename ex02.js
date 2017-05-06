2b55a6c57d15254c3c91b8f859a10b202874dabd
Jim Hall <jim.hall@solarmedia.com>
2017-05-06 13:02:14 -0400

    Exercise 2

---
+++ ex02.js
@@ -0,0 +1,22 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.route([
+    {
+        method: 'GET',
+        path: '/{name}',
+        handler: function(request, reply) {
+            reply('Hello ' + request.params.name);
+        }
+    }
+]);
+
+server.start((err) => {
+    if (err) throw err;
+   console.log('Server listening at:', server.info.uri);
+});
